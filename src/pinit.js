4d1a077af65eb31db65bc28b75fc6ee15633e013
Christophe Vimbert <c.vimbert@outlook.fr>
2016-05-31 09:03:39 +0200

    Dev

--- src/pinit.js
+++ src/pinit.js
@@ -38,9 +38,9 @@
             this.point.on("change", onPointChanged);
 
             // écoute de l'élément lié dans le DOM
-            $element.get(0).addEventListener("DOMSubtreeModified", onElementChanged);
-            $element.on("DOMElementChanged", onElementChanged);
-            $element.append("ok3");
+            // DOMAttrModified
+            $element.get(0).addEventListener("DOMAttrModified", onElementChanged);
+            //$element.on("DOMSubtreeModified", onElementChanged);
 
             function onPointChanged(e) {
                 console.log("Point changed:");
